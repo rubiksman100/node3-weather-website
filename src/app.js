2a293a90f0e64fd1bc402612818ce6227ce40aae
unknown <hallchri100@gmail.com>
2019-12-09 11:28:52 -0500

    =Set up for jeroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const app = express()
 const geocode = require('./util/geocode')
 const forecast = require('./util/forecast')
 
+const port = process.env.PORT || 3000
 
 
 //Define paths for express config
@@ -90,6 +91,6 @@ app.get('*',(req,res)=>{
         title:'404'})
 })
 
-app.listen(3000, ()=>{
-    console.log('Server is up on port 3000')
+app.listen(port, ()=>{
+    console.log('Server is up on port '+ port)
 })
